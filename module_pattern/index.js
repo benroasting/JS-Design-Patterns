c508f844ce88d036d65b472179cf3242ebad2825
Ben Young <ben.young@ramseysolutions.com>
2022-12-21 08:00:22 -0600

    first design patterns

---
+++ module_pattern/index.js
@@ -0,0 +1,20 @@
+export function sum(x, y) {
+  return x + y;
+}
+
+export function multiply(x, y) {
+  return x * y;
+}
+
+export function subtract(x, y) {
+  return x - y;
+}
+
+export function divide(x, y) {
+  return x / y;
+}
+
+console.log("Sum", sum(1, 2));
+console.log("Subtract", subtract(1, 2));
+console.log("Divide", divide(1, 2));
+console.log("Multiply", multiply(1, 2));
---
+++ singleton_pattern/classCounter.js
@@ -0,0 +1,27 @@
+let instance;
+
+class Counter {
+  constructor() {
+    if (instance) {
+      throw new Error("You can only create one instance!");
+    }
+    this.counter = counter;
+    instance = this;
+  }
+
+  getCount() {
+    return this.counter;
+  }
+
+  increment() {
+    return ++this.counter;
+  }
+
+  decrement() {
+    return --this.counter;
+  }
+}
+
+const singletonCounter = Object.freeze(new Counter());
+
+export default singletonCounter;
