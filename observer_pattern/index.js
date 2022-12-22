26d8630e6ebad10f6b6237a68fc5f6be7f6c0000
Ben Young <ben.young@ramseysolutions.com>
2022-12-22 15:38:00 -0600

    observer pattern

---
+++ observer_pattern/index.js
@@ -0,0 +1,16 @@
+import ".style.css";
+import "./analytics.js";
+import Observable from "./observable.js";
+
+const pinkBtn = document.getElementById("pink-btn");
+const blueBtn = document.getElementById("blue-btn");
+
+pinkBtn.addEventListener("click", () => {
+  const data = "🎀 Click on pink button! 🎀";
+  Observable.notify(data);
+});
+
+blueBtn.addEventListener("click", () => {
+  const data = "🦋 Click on blue button! 🦋";
+  Observable.notify(data);
+});
